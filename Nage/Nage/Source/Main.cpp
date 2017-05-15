d2eac0830c0c3ca373ff03246aea3decebac6c9f
Daniel Cordell <danielbc@hotmail.co.uk>
2017-05-16 00:07:50 +0100

    Now draws a rectangle

--- Nage/Nage/Source/Main.cpp
+++ Nage/Nage/Source/Main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
 
 int main() {
-	std::cout << "Test";
+	sf::RectangleShape rect(sf::Vector2f(50,40));
+
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Test");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			default: 
+				break;
+			}
+		}
+
+		window.clear(sf::Color::Black);
+		window.draw(rect);
+		window.display();
+
+	}
 }
