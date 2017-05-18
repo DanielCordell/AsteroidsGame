654f4ee600bef4563d30a9a5fa4259697fb31767
Daniel Cordell <danielbc@hotmail.co.uk>
2017-05-18 23:49:29 +0100

    They Work!

--- Nage/Nage/Source/Includes/IResourceManager.h
+++ Nage/Nage/Source/Includes/IResourceManager.h
@@ -11,15 +11,26 @@ private:
 public:
 	T& Get(ID id) {
 		auto data = Map.find(id);
-		if (data == Map.end) throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(T).name()) + " - Trying to get Resource of ID " + std::to_string(int(id)) + " that does not exist");
+		if (data == Map.end()) throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(T).name()) + " - Trying to get Resource of ID " + std::to_string(int(id)) + " that does not exist");
 		return *(data->second);
 	}
 	void Load(ID id, const std::string& filename) {
-		if (Map.find(id) == Map.end())
+		if (Map.find(id) != Map.end())
 			throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(T).name()) + " - Trying to load Resource with ID that already exists " + std::to_string(int(id)) + " while loading" + filename);
 		auto resource = std::make_shared<T>();
 		if (!resource->loadFromFile(filename))
 			throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(T).name()) + " - Failed to Load " + filename);
+		Map.insert(std::make_pair(id, resource));
+	}
+	template <typename Parameter>
+	void Load(ID id, const std::string& filename, Parameter& secondParam) {
+		if (Map.find(id) != Map.end())
+			throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(sf::Shader).name()) + " - Trying to load Resource with ID that already exists " + std::to_string(int(id)) + " while loading" + filename);
+		auto resource = std::make_shared<sf::Shader>();
+
+
+		if (!resource->loadFromFile(filename, secondParam))
+			throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(sf::Shader).name()) + " - Failed to Load " + filename);
 		Map.insert(std::make_pair(id, resource));
 
 	}
@@ -33,7 +44,7 @@ enum class IDFont { one };
 
 template<>
 inline void IResourceManager<sf::Music, IDMusic>::Load(IDMusic id, const std::string& filename) {
-	if (Map.find(id) == Map.end())
+	if (Map.find(id) != Map.end())
 		throw std::runtime_error("Error in ResourceHolder of Type: " + std::string(typeid(sf::Music).name()) + " - Trying to load Resource with ID that already exists " + std::to_string(int(id)) + " while loading" + filename);
 	auto resource = std::make_shared<sf::Music>();
 	if (!resource->openFromFile(filename))
--- Nage/Nage/Source/Main.cpp
+++ Nage/Nage/Source/Main.cpp
@@ -2,6 +2,9 @@
 #include "Includes/EngineLogger.h"
 #include "Includes/IResourceManager.h"
 
+#include "SFML/Audio/Sound.hpp"
+#include "SFML/Audio/SoundBuffer.hpp"
+
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Test");
 
@@ -9,8 +12,36 @@ int main() {
 
 	EngineLogger::Log(EngineLogger::LOG_INFO, "Testing TextureManager");
 	IResourceManager<sf::Texture, IDTexture> TexManager;
-	TexManager.Load(IDTexture::one,"Resources/test.png");
+	TexManager.Load(IDTexture::one,"Resources/Test.png");
+	sf::Sprite sprite(TexManager.Get(IDTexture::one));
+	sprite.setPosition(500, 300);
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Success");
 
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Testing SoundManager");
+	IResourceManager<sf::SoundBuffer, IDSound> SoundManager;
+	SoundManager.Load(IDSound::one, "Resources/Test.wav");
+	sf::Sound sound(SoundManager.Get(IDSound::one));
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Success");
+
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Testing MusicManager");
+	IResourceManager<sf::Music, IDMusic> MusicManager;
+	MusicManager.Load(IDMusic::one, "Resources/Test2.wav");
+	sf::Music & music(MusicManager.Get(IDMusic::one));
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Success");
+
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Testing FontManager");
+	IResourceManager<sf::Font, IDFont> FontManager;
+	FontManager.Load(IDFont::one, "Resources/Test.ttf");
+	sf::Text text("OH HELLO THERE", FontManager.Get(IDFont::one));
+	text.setPosition(200, 200);
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Success");
+
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Testing ShaderManager");
+	IResourceManager<sf::Shader, IDShader> ShaderManager;
+	auto test = sf::Shader::Type::Fragment;
+	ShaderManager.Load(IDShader::one, "Resources/Test.glsl", test);
+	sf::Shader & shader(ShaderManager.Get(IDShader::one));
+	EngineLogger::Log(EngineLogger::LOG_INFO, "Success");
 
 
 	while (window.isOpen()) {
@@ -20,12 +51,23 @@ int main() {
 			case sf::Event::Closed:
 				window.close();
 				break;
+			case sf::Event::MouseButtonPressed:
+				if (event.mouseButton.button == sf::Mouse::Left) {
+					sound.play();
+					music.stop();
+				}
+				else if (event.mouseButton.button == sf::Mouse::Right) {
+					music.play();
+				}
+				break;
 			default: 
 				break;
 			}
 		}
 
 		window.clear(sf::Color::Black);
+		window.draw(text);
+		window.draw(sprite,&ShaderManager.Get(IDShader::one));
 		window.display();
 
 	}
